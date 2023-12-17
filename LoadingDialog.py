632a9102147e9101eeacf2b0d6a35e29e8f5bc5e
Bahador Rousta <bahador.rj@gmail.com>
2023-12-17 21:28:35 +0330

    use animated text

--- LoadingDialog.py
+++ LoadingDialog.py
@@ -1,12 +1,12 @@
 import pyqtspinner
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 
 
 class Window(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         self.layout = QtWidgets.QHBoxLayout(self)
-        self.spinner = pyqtspinner.WaitingSpinner(self, False, radius=20)
+        self.spinner = pyqtspinner.WaitingSpinner(self, False, radius=15)
         self.spacerOne = QtWidgets.QSpacerItem(
             0, 0, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum
         )
@@ -16,11 +16,11 @@ class Window(QtWidgets.QDialog):
         self.label = QtWidgets.QLabel()
 
     def setup_ui(self):
-        self.setFixedSize(QtCore.QSize(300, 200))
-        self.setWindowTitle("Loading")
-        self.label.setText("Collecting elements...")
+        self.setFixedWidth(300)
+        self.setWindowTitle("Please wait")
         self.layout.addWidget(self.spinner)
         self.layout.addItem(self.spacerOne)
         self.layout.addWidget(self.label)
         self.layout.addItem(self.spacerTwo)
+        self.label.setText("Collecting elements")
         self.spinner.start()
