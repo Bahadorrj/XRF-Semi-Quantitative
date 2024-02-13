f183e19fe8767a73763c58c85ca30198dba67665
Bahador Rousta <bahador.rj@gmail.com>
2024-02-13 11:33:45 +0330

    add attrs to Database class

--- src/main/python/Logic/Sqlite.py
+++ src/main/python/Logic/Sqlite.py
@@ -1,41 +1,37 @@
 import sqlite3
-from pathlib import Path
-
 import pandas as pd
 
-ADDRESS = {
-    "fundamentals": r"DB\fundamentals.db"
-}
+from attrs import define, field, setters
 
 
+@define(order=True)
 class Database:
-    def __init__(self, address):
-        self._address = address
-        self._name = Path(address).stem
-        self._connection = None
-        self.initConnection()
+    address: str = field(on_setattr=setters.frozen)
+    name: str = field(on_setattr=setters.frozen, default="main")
+    connection: sqlite3.Connection = field(init=False)
 
-    def getConnection(self):
-        return self._connection
-
-    def setConnection(self, connection):
-        self._connection = connection
-
-    def initConnection(self):
-        if self.getConnection() is None:
-            self.setConnection(sqlite3.connect(self._address))
+    def connect(self):
+        self.connection = sqlite3.connect(self.address)
 
     def closeConnection(self):
-        if self.getConnection() is not None:
-            self.getConnection().close()
+        if self.connection is not None:
+            self.connection.close()
 
 
-DATABASES = {"fundamentals": Database(ADDRESS["fundamentals"])}
+ADDRESS = {
+    "fundamentals": r"DB/fundamentals.db"
+}
+DATABASES = {
+    "fundamentals": Database(ADDRESS["fundamentals"])
+}
+
+DATABASES["fundamentals"].connect()
+print(DATABASES)
 
 
 def getColumnLabels(databaseName, tableName):
     # Connect to the SQLite database
-    connection = DATABASES.get(databaseName).getConnection()
+    connection = DATABASES.get(databaseName).connection
     cursor = connection.cursor()
     # Get column labels from the specified table
     cursor.execute(f"PRAGMA table_info({tableName})")
@@ -46,7 +42,7 @@ def getColumnLabels(databaseName, tableName):
 
 
 def getValue(databaseName, tableName, columnName="*", where=""):
-    connection = DATABASES.get(databaseName).getConnection()
+    connection = DATABASES.get(databaseName).connection
     cur = connection.cursor()
     query = "SELECT " + columnName + " FROM " + tableName + " " + where + ";"
     cur.execute(query)
@@ -55,7 +51,7 @@ def getValue(databaseName, tableName, columnName="*", where=""):
 
 
 def getValues(databaseName, tableName, columnName="*", where=""):
-    connection = DATABASES.get(databaseName).getConnection()
+    connection = DATABASES.get(databaseName).connection
     cur = connection.cursor()
     query = "SELECT " + columnName + " FROM " + tableName + " " + where + ";"
     cur.execute(query)
@@ -64,7 +60,7 @@ def getValues(databaseName, tableName, columnName="*", where=""):
 
 
 def getDatabaseDataframe(databaseName, tableName, columnName='*', where=''):
-    connection = DATABASES.get(databaseName).getConnection()
+    connection = DATABASES.get(databaseName).connection
     if where == '':
         dataframe = pd.read_sql_query(f"SELECT {columnName} FROM {tableName}", connection)
     else:
