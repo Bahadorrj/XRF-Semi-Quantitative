d06355ca118656ebacf5f9be5a812bc30dc900d6
Bahador Rousta <bahador.jorshary@gmail.com>
2024-02-28 12:16:33 +0330

    remove _initElements method and its usage in __post_init__

--- src/main/python/Types/ConditionClass.py
+++ src/main/python/Types/ConditionClass.py
@@ -8,19 +8,7 @@ from src.main.python.Types.ElementClass import Element
 @dataclass(order=True)
 class Condition(Data):
     id: int
-    elements: list[Element] = field(default_factory=list, init=False)
 
     def __post_init__(self):
         self.labels = getColumnLabels("fundamentals", "conditions")
-        self.values = getValue("fundamentals", "conditions", where=f"WHERE condition_id = '{self.id}'")
-        self._initElements()
-
-    def _initElements(self):
-        values = getValues(
-                "fundamentals",
-                "elements",
-                where=f"WHERE condition_id = {self.id} AND active == 1"
-        )
-        for value in values:
-            e = Element(value[0])
-            self.elements.append(e)
+        self.values = getValue("fundamentals", "conditions", where=f"condition_id = '{self.id}'")
