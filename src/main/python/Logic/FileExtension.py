235f94495be9f0562c67de19cdb7de51bfbf21ad
Bahador Rousta <bahador.jorshary@gmail.com>
2024-03-06 10:42:50 +0330

    add FileExtension for handling saving and opening self defined extensions

---
+++ src/main/python/Logic/FileExtension.py
@@ -0,0 +1,43 @@
+from json import dump, load
+from numpy import array, uint32
+
+from src.main.python.Types.FileClass import File
+from src.main.python.Types.ConditionClass import Condition
+
+
+class FileHandler:
+    @staticmethod
+    def writeFiles(files, filePath):
+        # Serialize each LocalFile object to a dictionary
+        filesDictList = []
+        for file in files:
+            countsSerializable = [countArray.tolist() for countArray in file.counts]
+            idsSerializable = [condition.id for condition in file.conditions]
+            localFileDict = {
+                "name": file.name,
+                "ids": idsSerializable,
+                "counts": countsSerializable
+            }
+            filesDictList.append(localFileDict)
+
+        # Write serialized data to a file
+        with open(filePath, 'w') as f:
+            dump(filesDictList, f, indent=4)
+
+    @staticmethod
+    def readFiles(filePath):
+        # Read serialized data from a file
+        with open(filePath, 'r') as f:
+            filesAsDict = load(f)
+
+        files = []
+        for fileAsDict in filesAsDict:
+            name = fileAsDict["name"]
+            countsArray = [array(countArray, dtype=uint32) for countArray in fileAsDict['counts']]
+            ids = fileAsDict['ids']
+
+            file = File(name)
+            file.conditions = [Condition(id) for id in ids]
+            file.counts = countsArray
+            files.append(file)
+        return files
