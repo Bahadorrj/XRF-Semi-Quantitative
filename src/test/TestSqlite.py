350d7a0dd871398f6393e47e6e73b5da196defb7
Bahador Rousta <bahador.jorshary@gmail.com>
2024-03-26 14:07:20 +0330

    improve TestSqlite.py

--- src/test/TestSqlite.py
+++ src/test/TestSqlite.py
@@ -1,37 +1,29 @@
 import unittest
 
 from python.Logic.Sqlite import DatabaseConnection
-from src.main.python.Logic import Sqlite
 
 
 class TestSqlite(unittest.TestCase):
     def setUp(self):
         self.database = DatabaseConnection.getInstance(":fundamentals.db")
 
-    def test_get_column_labels(self):
-        labels = ['condition_id', 'name', 'Kv', 'mA', 'time', 'rotation', 'environment', 'filter', 'mask', 'active']
-        self.assertEqual(
-            Sqlite.getColumnLabels(self.database, "conditions"),
-            labels,
-            "Column labels do not match expected labels."
-        )
+    def test_connection(self):
+        self.assertIsNotNone(self.database.conn, "Database connection was not established")
 
-    def test_get_value(self):
-        self.assertEqual(
-            Sqlite.getValue(self.database, 'elements', 'radiation_type', 'element_id = 4'),
-            ('Ka',),
-            "Value is not correctly selected."
-        )
+    def test_conditions_existence(self):
+        # Check if the table exists
+        result = self.database.executeQuery(f"SELECT name FROM sqlite_master WHERE type='table' AND name='conditions'")
+        # Assert whether the table exists or not
+        self.assertIsNotNone(result.fetchone(), f"Table 'conditions' does not exist")
 
-    def test_get_values(self):
-        self.assertEqual(
-            Sqlite.getValues(self.database, 'elements', 'element_id', "name = 'Silicon'"),
-            [(15,), (16,), (412,)],
-            "Value is not correctly selected."
-        )
+    def test_elements_existence(self):
+        # Check if the table exists
+        result = self.database.executeQuery(f"SELECT name FROM sqlite_master WHERE type='table' AND name='elements'")
+        # Assert whether the table exists or not
+        self.assertIsNotNone(result.fetchone(), f"Table 'elements' does not exist")
 
-    def test_get_database_dataframe(self):
-        self.assertIsNotNone(
-            Sqlite.getDatabaseDataframe(self.database, 'elements', 'radiation_type'),
-            msg="No dataframe was returned"
-        )
+    def test_UQ_existence(self):
+        # Check if the table exists
+        result = self.database.executeQuery(f"SELECT name FROM sqlite_master WHERE type='table' AND name='UQ'")
+        # Assert whether the table exists or not
+        self.assertIsNotNone(result.fetchone(), f"Table 'UQ' does not exist")
