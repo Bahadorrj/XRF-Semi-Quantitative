c939a6bf5cd06872a26ef7998e9f99085836ca56
Bahador Rousta <bahador.jorshary@gmail.com>
2024-08-26 16:06:17 +0330

    Revert "Add FormDialog class to base classes"
    This reverts commit 2af693d328d0e914a8b75ccacb41ec9fc7a53e51.

--- python/views/trays/calibrationtray.py
+++ python/views/trays/calibrationtray.py
@@ -1,12 +1,14 @@
 import os
+from functools import partial
+from json import dumps
 from pathlib import Path
 
 import pandas
-from PyQt6 import QtCore, QtWidgets
+from PyQt6 import QtCore, QtGui, QtWidgets
 
+from python.utils import encryption
 from python.utils.database import getDataframe, getDatabase, reloadDataframes
 from python.utils.datatypes import Calibration, Analyse
-from python.views.base.formdialog import FormDialog
 from python.views.base.tablewidget import TableItem
 from python.views.base.traywidget import TrayWidget
 from python.views.explorers.calibrationexplorer import CalibrationExplorer
@@ -15,18 +17,48 @@ from python.views.widgets.generaldatawidget import GeneralDataWidget
 from python.views.widgets.linestablewidget import LinesTableWidget
 
 
-class CalibrationFormDialog(FormDialog):
+class FormDialog(QtWidgets.QDialog):
     def __init__(self,
                  parent: QtWidgets.QWidget | None = None,
                  inputs: list | tuple | None = None,
                  values: list | tuple | None = None) -> None:
-        super(CalibrationFormDialog, self).__init__(parent, inputs, values)
+        super(FormDialog, self).__init__(parent)
+        self.setFixedSize(250, 200)
+        if inputs is not None:
+            self._fields = {key: "" for key in inputs}
+            if values is not None:
+                for key, value in zip(inputs, values):
+                    self._fields[key] = value
+            self._initializeUi()
+
+    def _initializeUi(self) -> None:
+        mainLayout = QtWidgets.QVBoxLayout()
+        for key, value in self._fields.items():
+            label = QtWidgets.QLabel(f"{key}:")
+            lineEdit = QtWidgets.QLineEdit(value)
+            if key == "concentration":
+                lineEdit.setValidator(QtGui.QDoubleValidator())
+            lineEdit.setFixedWidth(100)
+            lineEdit.editingFinished.connect(partial(self._fill, lineEdit, key))
+            layout = QtWidgets.QHBoxLayout()
+            layout.addWidget(label)
+            layout.addWidget(lineEdit)
+            mainLayout.addLayout(layout)
+        mainLayout.addStretch()
+        self._errorLabel = QtWidgets.QLabel()
+        self._errorLabel.setStyleSheet("color: red;")
+        mainLayout.addWidget(self._errorLabel)
+        buttonBox = QtWidgets.QDialogButtonBox()
+        buttonBox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.StandardButton.Cancel | QtWidgets.QDialogButtonBox.StandardButton.Ok
+        )
+        buttonBox.accepted.connect(self._check)
+        buttonBox.rejected.connect(self.reject)
+        mainLayout.addWidget(buttonBox)
+        self.setLayout(mainLayout)
+        self.setModal(True)
 
     def _fill(self, lineEdit: QtWidgets.QLineEdit, key: str) -> None:
-        if key == "filename":
-            calibrationPath = f"calibrations/{lineEdit.text()}.atxc"
-            if os.path.exists(calibrationPath):
-                self._errorLabel.setText("This filename already exists!")
         if key == "element":
             if not lineEdit.text() in getDataframe("Lines")["symbol"].values:
                 lineEdit.setStyleSheet("color: red;")
@@ -35,11 +67,21 @@ class CalibrationFormDialog(FormDialog):
         elif key == "concentration":
             if not 0 <= float(lineEdit.text()) <= 100:
                 lineEdit.setStyleSheet("color: red;")
-                self._errorLabel.setText("Concentration must be between 0 and 100!")
+                self._errorLabel.setText("concentration must be between 0 and 100!")
                 return
         lineEdit.setStyleSheet("color: black;")
         self._errorLabel.setText(None)
-        return super()._fill(lineEdit, key)
+        self._fields[key] = lineEdit.text()
+
+    def _check(self) -> None:
+        if all(value != "" for value in self._fields.values()):
+            self.accept()
+        else:
+            QtWidgets.QApplication.beep()
+
+    @property
+    def fields(self):
+        return self._fields
 
 
 class CalibrationTrayWidget(TrayWidget):
@@ -54,10 +96,12 @@ class CalibrationTrayWidget(TrayWidget):
             self._connectSignalsAndSlots()
             if self._df.empty is False:
                 self._tableWidget.setCurrentCell(0, 0)
+                self._actionsMap["remove"].setDisabled(False)
+                self._actionsMap["edit"].setDisabled(False)
 
     def _initializeUi(self) -> None:
         self.setWindowTitle("Calibration Tray List")
-        self.setObjectName("tray-list")
+        self.setObjectName("calibration-tray-list")
         self._createActions({
             "Add": False,
             "Remove": True,
@@ -76,7 +120,7 @@ class CalibrationTrayWidget(TrayWidget):
         self._createTabWidget()
         self._setUpView()
 
-    def _resetClassVariables(self, dataframe: pandas.DataFrame) -> None:
+    def _resetVariables(self, dataframe: pandas.DataFrame) -> None:
         self._df = dataframe
 
     @QtCore.pyqtSlot(str)
@@ -95,31 +139,36 @@ class CalibrationTrayWidget(TrayWidget):
 
     def _addCalibration(self) -> None:
         # Ask the user for the filename, element, concentration and create a new calibration
-        addDialog = CalibrationFormDialog(inputs=self._df.columns[1:-1])
-        addDialog.setWindowTitle("Add calibration")
+        addDialog = FormDialog(inputs=self._df.columns[1:-1])
         if addDialog.exec():
             filename = addDialog.fields["filename"]
             element = addDialog.fields["element"]
             concentration = float(addDialog.fields["concentration"])
+            status = "Proceed to acquisition"
             # Find the default radiation type for the element
             if not getDataframe("Lines").query(f"symbol == '{element}' and active == 1").empty:
-                self._calibration = Calibration(filename, element, concentration)
-                self._calibration.save()
+                calibrationPath = f"calibrations/{filename}.atxc"
+                if os.path.exists(calibrationPath):
+                    messageBox = QtWidgets.QMessageBox()
+                    messageBox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
+                    messageBox.setText("The selected name already exists in the database.")
+                    messageBox.setWindowTitle("Add Calibration Failed")
+                    messageBox.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
+                    messageBox.exec()
+                    return self._addCalibration()
+                self._calibration = Calibration(element, concentration)
+                self._saveCalibrationFile(calibrationPath)
                 # Insert the calibration into the database
-                self._insertCalibration()
+                self._insertCalibration(filename, element, concentration, status)
+                self._actionsMap["edit"].setDisabled(False)
 
-    def _insertCalibration(self) -> None:
-        filename = self._calibration.filename
-        element = self._calibration.element
-        concentration = self._calibration.concentration
-        state = self._calibration.state
-        status = self._calibration.status()
+    def _insertCalibration(self, filename: str, element: str, concentration: float, status: str) -> None:
         getDatabase().executeQuery(
-            "INSERT INTO Calibrations (filename, element, concentration, state) VALUES (?, ?, ?, ?)",
-            (filename, element, concentration, state)
+            "INSERT INTO Calibrations (filename, element, concentration, status) VALUES (?, ?, ?, ?)",
+            (filename, element, concentration, status)
         )
         reloadDataframes()
-        self._resetClassVariables(getDataframe("Calibrations"))
+        self._resetVariables(getDataframe("Calibrations"))
         items = {
             "filename": TableItem(filename),
             "element": TableItem(element),
@@ -128,26 +177,43 @@ class CalibrationTrayWidget(TrayWidget):
         }
         self._tableWidget.addRow(items)
         self._tableWidget.setCurrentCell(self._tableWidget.rowCount() - 1, 0)
+        # Enable the remove action if there are more than one calibration
+        if not self._actionsMap["remove"].isEnabled():
+            self._actionsMap["remove"].setDisabled(False)
+
+    def _saveCalibrationFile(self, calibrationPath: str) -> None:
+        with open(calibrationPath, "wb") as f:
+            key = encryption.loadKey()
+            jsonText = dumps(self._calibration.toHashableDict())
+            encryptedText = encryption.encryptText(jsonText, key)
+            f.write(encryptedText + b"\n")
 
     def _editCalibration(self) -> None:
         previousFilename = self._tableWidget.getCurrentRow()["filename"].text()
         previousElement = self._tableWidget.getCurrentRow()["element"].text()
         previousConcentration = self._tableWidget.getCurrentRow()["concentration"].text()
-        editDialog = CalibrationFormDialog(
+        editDialog = FormDialog(
             inputs=self._df.columns[1:-1],
             values=(previousFilename, previousElement, previousConcentration)
         )
-        editDialog.setWindowTitle("Edit calibration")
         if editDialog.exec():
             filename = editDialog.fields["filename"]
             element = editDialog.fields["element"]
             concentration = float(editDialog.fields["concentration"])
             # Find the default radiation type for the element
             if not getDataframe("Lines").query(f"symbol == '{element}' and active == 1").empty:
-                self._calibration.filename = filename
+                calibrationPath = f"calibrations/{filename}.atxc"
+                if os.path.exists(calibrationPath):
+                    messageBox = QtWidgets.QMessageBox()
+                    messageBox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
+                    messageBox.setText("The selected name already exists in the database.")
+                    messageBox.setWindowTitle("Add Calibration Failed")
+                    messageBox.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
+                    messageBox.exec()
+                    return self._addCalibration()
                 self._calibration.element = element
                 self._calibration.concentration = concentration
-                self._calibration.save()
+                self._saveCalibrationFile(f"calibrations/{filename}.atxc")
                 self._tableWidget.getCurrentRow()["filename"].setText(filename)
                 self._tableWidget.getCurrentRow()["element"].setText(element)
                 self._tableWidget.getCurrentRow()["concentration"].setText(f"{concentration:.1f}")
@@ -157,7 +223,7 @@ class CalibrationTrayWidget(TrayWidget):
                     f"WHERE filename = '{previousFilename}'"
                 )
                 reloadDataframes()
-                self._resetClassVariables(getDataframe("Calibrations"))
+                self._resetVariables(getDataframe("Calibrations"))
 
     def _openCalibrationExplorer(self) -> None:
         if self._calibration is not None:
@@ -177,10 +243,9 @@ class CalibrationTrayWidget(TrayWidget):
         if message_box.exec() == QtWidgets.QMessageBox.StandardButton.Yes:
             filename = self._tableWidget.getRow(self._tableWidget.currentRow())["filename"].text()
             os.remove(f"calibrations/{filename}.atxc")
-            self._calibration = None
             getDatabase().executeQuery("DELETE FROM Calibrations WHERE filename = ?", (filename,))
             reloadDataframes()
-            self._resetClassVariables(getDataframe("Calibrations"))
+            self._resetVariables(getDataframe("Calibrations"))
             self._tableWidget.removeRow(self._tableWidget.currentRow())
 
     def _importCalibration(self) -> None:
@@ -194,7 +259,15 @@ class CalibrationTrayWidget(TrayWidget):
             self._calibration = Calibration.fromATXCFile(filePath)
             filename = Path(filePath).stem
             if self._df.query(f"filename == '{filename}'").empty:
-                self._insertCalibration()
+                element = self._calibration.element
+                concentration = self._calibration.concentration
+                if self._calibration.state == 0:
+                    status = "Proceed to acquisition"
+                elif self._calibration.state == 1:
+                    status = "Initial state"
+                else:
+                    status = "Edited by user"
+                self._insertCalibration(filename, element, concentration, status)
             else:
                 messageBox = QtWidgets.QMessageBox()
                 messageBox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
@@ -202,6 +275,17 @@ class CalibrationTrayWidget(TrayWidget):
                 messageBox.setWindowTitle("Import Calibration Failed")
                 messageBox.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
                 messageBox.exec()
+
+    def _calculateCoefficients(self):
+        df = self._calibration.lines.query(
+            f"symbol == '{self._calibration.element}' and active == 1"
+        )
+        for row in df.itertuples(index=False):
+            data = self._calibration.analyse.getDataByConditionId(row.condition_id)
+            intensity = data.calculateIntensities(
+                self._calibration.lines
+            )[self._calibration.element][row.radiation_type]
+            self._calibration.coefficients[row.radiation_type] = self._calibration.concentration / intensity
 
     def _fillMenusWithActions(self) -> None:
         self._menusMap["file"].addAction(self._actionsMap["import"])
@@ -241,8 +325,6 @@ class CalibrationTrayWidget(TrayWidget):
             filename = tableRow.get("filename").text()
             path = f"./calibrations/{filename}.atxc"
             self._calibration = Calibration.fromATXCFile(path)
-            self._actionsMap["edit"].setDisabled(False)
-            self._actionsMap["remove"].setDisabled(False)
             if self._calibration.state == 0:
                 self._addAcquisitionWidget()
             else:
@@ -251,6 +333,8 @@ class CalibrationTrayWidget(TrayWidget):
             self._widgets.clear()
             self._tabWidget.clear()
             self._calibration = None
+            self._actionsMap["edit"].setDisabled(True)
+            self._actionsMap["remove"].setDisabled(True)
 
     def _addAcquisitionWidget(self):
         widget = QtWidgets.QWidget()
@@ -292,12 +376,15 @@ class CalibrationTrayWidget(TrayWidget):
             analyse = Analyse.fromTXTFile(path) if path.endswith(".txt") else Analyse.fromATXFile(path)
             self._calibration.analyse = analyse
             self._calibration.state = 1
-            self._calibration.save()
+            self._calculateCoefficients()
+            tableRow = self._tableWidget.getRow(self._tableWidget.currentRow())
+            calibrationPath = f"./calibrations/{tableRow.get('filename').text()}.atxc"
+            calibrationId = self._df.at[self._tableWidget.currentRow(), "calibration_id"]
+            self._saveCalibrationFile(calibrationPath)
             getDatabase().executeQuery(
-                f"UPDATE Calibrations SET state = 1 WHERE filename = '{self._calibration.filename}'"
+                f"UPDATE Calibrations SET status = 'Initial state' WHERE calibration_id = {calibrationId}"
             )
             reloadDataframes()
-            self._resetClassVariables(getDataframe("Calibrations"))
-            tableRow = self._tableWidget.getCurrentRow()
-            tableRow.get("status").setText(self._calibration.status())
+            self._resetVariables(getDataframe("Calibrations"))
+            tableRow.get("status").setText("Initial state")
             self._addCalibrationWidgets()
